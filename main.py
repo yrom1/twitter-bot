b5cb43b98e2f0d730db530542bac17e5a1b14bf3
Ryan Moore <ryanm.inbox@gmail.com>
2022-12-17 00:12:11 -0500

    print a tweet

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ consumer_secret = environ["TWITTER_BOT_API_KEY_SECRET"]
 access_token = environ["TWITTER_BOT_ACCESS_TOKEN"]
 access_token_secret = environ["TWITTER_BOT_ACCESS_TOKEN_SECRET"]
 
-# Authenticate with the Twitter API
+
 auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
-
-# Create a tweepy API client
 api = tweepy.API(auth)
+# api.update_status('Hello, world!') # need ELEVATED PRIVILEGES
 
-# Post a tweet
-api.update_status('Hello, world!')
+user = "geoffreyhinton"
+for status in tweepy.Cursor(api.user_timeline, screen_name=user, tweet_mode="extended").items():
+    print(status.full_text)
+    break
