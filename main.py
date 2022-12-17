119d6cde3a74a2997b8f98989542abd5467762c8
Ryan Moore <ryanm.inbox@gmail.com>
2022-12-16 22:23:55 -0500

    write access error: ``` Traceback (most recent call last):   File "/Users/ryan/twitter-bot/main.py", line 17, in <module>     api.update_status('Hello, world!')   File "/Users/ryan/.pyenv/versions/3.10.8/lib/python3.10/site-packages/tweepy/api.py", line 46, in wrapper     return method(*args, **kwargs)   File "/Users/ryan/.pyenv/versions/3.10.8/lib/python3.10/site-packages/tweepy/api.py", line 1142, in update_status     return self.request(   File "/Users/ryan/.pyenv/versions/3.10.8/lib/python3.10/site-packages/tweepy/api.py", line 265, in request     raise Forbidden(resp) tweepy.errors.Forbidden: 403 Forbidden 453 - You currently have Essential access which includes access to Twitter API v2 endpoints only. If you need access to this endpoint, you’ll need to apply for Elevated access via the Developer Portal. You can learn more here: https://developer.twitter.com/en/docs/twitter-api/getting-started/about-twitter-api#v2-access-leve ```

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hi")
+from os import environ
+
+import tweepy
+
+consumer_key = environ["TWITTER_BOT_API_KEY"]
+consumer_secret = environ["TWITTER_BOT_API_KEY_SECRET"]
+access_token = environ["TWITTER_BOT_ACCESS_TOKEN"]
+access_token_secret = environ["TWITTER_BOT_ACCESS_TOKEN_SECRET"]
+
+# Authenticate with the Twitter API
+auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
+
+# Create a tweepy API client
+api = tweepy.API(auth)
+
+# Post a tweet
+api.update_status('Hello, world!')
--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
+source ~/PRIVATE/TWITTER_BOT_PRIVATE.sh
 python3 -m pip install -r dev-requirements.txt
 python3 main.py
